df3c83549beaca892c53399d7551ca12a148c404
Ricardo Torres <giron.torres1@gmail.com>
2024-02-11 23:57:02 -0600

    clase Nodo

---
+++ GASOLINERA2/cNodo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GASOLINERA2
+{
+    class cNodo
+    {
+        
+        private string dato;
+
+        private cNodo siguiente = null;
+
+        public string Dato { get => dato; set => dato = value; }
+
+        internal cNodo Siguiente { get => siguiente; set => siguiente = value; }
+
+        public override string ToString()
+        {
+            return string.Format("[{0}]", dato);
+        }
+    }
+}
